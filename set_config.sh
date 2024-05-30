dd6a8cc924efc69da576e8761721e4cd93e13784
funkyFangs <funkyFangs@outlook.com>
2024-05-29 19:21:11 -0500

    fix script issues

--- set_config.sh
+++ set_config.sh
@@ -5,4 +5,4 @@ VALUE=$2
 TARGET=$3
 
 # Source: https://superuser.com/a/976712
-sed "/^${KEY}=/{h;s/=.*/=${VALUE}/};\${x;/^\${s//${KEY}=${VALUE}/;H};x}" $TARGET
+sed -i "/^${KEY}=/{h;s/=.*/=${VALUE}/};\${x;/^\$/{s//${KEY}=${VALUE}/;H};x}" $TARGET
